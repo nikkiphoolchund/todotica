f6a8d8e38db278867c10c55061c69c328e84936b
Remi Marenco <remi.marenco@gmail.com>
2016-02-28 21:59:58 -0500

    Add api_habitica to return Task objects (Although a middle class should handle the conversion...)

--- api_habitica.py
+++ api_habitica.py
@@ -1,9 +1,12 @@
+# coding: utf8
 # Get all uncompleted tasks
 
 import requests
 import pprint
 
 import config
+
+from Task import Task
 
 url_habitica_api = "https://habitica.com/api/v2/"
 
@@ -17,11 +20,21 @@ headers = {
 
 
 def get_uncompleted():
+    tasks_uncompleted = []
+
     r = requests.get(''.join((url_habitica_api, 'user/tasks')), headers=headers)
     tasks_list = r.json()
     for task in tasks_list:
         if task['type'] == 'todo' and task['completed'] == False:
-            print task
+            new_task = Task(task['text'], task['dateCreated'])
+            tasks_uncompleted.append(new_task)
+
+            # print task
             # print task['text']
             # print ''.join(('    ', task['type']))
     # pprint.pprint( r.json() )
+    # print tasks_uncompleted
+    return tasks_uncompleted
+
+if __name__ == "__main__":
+    print get_uncompleted()
