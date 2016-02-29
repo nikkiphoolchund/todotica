1b05e1a865549334467e12349ef17fbbc88f2c69
Remi Marenco <remi.marenco@gmail.com>
2016-02-28 22:00:24 -0500

    Add api_todoist.py to return Task objects

--- api_todoist.py
+++ api_todoist.py
@@ -1,11 +1,13 @@
+# coding: utf8
 # Get all uncompleted tasks
 
 import todoist
 import json
 import inspect
 from datetime import datetime
-from dateutil.parser import parse
 import pprint
+
+from Task import Task
 
 import config
 #
@@ -14,6 +16,8 @@ import config
 #  TODO: Catch the error is error_tag: AUTH_INVALID_TOKEN
 
 def get_uncompleted():
+    tasks_uncompleted = []
+
     api = todoist.TodoistAPI(config.todoist_api_token)
     myTasks = api.query(['view all'])[0]
 
@@ -23,6 +27,14 @@ def get_uncompleted():
         print project['project_name']
         for task in project['uncompleted']:
             if task['checked'] == 0:
-                print " - " + task['content']
-            else:
-                print " - COMPLETED BUT STILL FETCHED: " + task['content']
+                # print " - " + task['content']
+                new_task = Task(task['content'], task['due_date_utc'])
+                # print u" - {0}".format(new_task)
+                tasks_uncompleted.append(new_task)
+            # else:
+                # print " - COMPLETED BUT STILL FETCHED: " + task['content']
+
+        return tasks_uncompleted
+
+if __name__ == "__main__":
+    print get_uncompleted()
