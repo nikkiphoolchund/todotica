2b43a5e780002b1cd538b4743270613a4b5e3142
Remi Marenco <remi.marenco@gmail.com>
2016-02-28 21:58:38 -0500

    Add Task as printable

--- Task.py
+++ Task.py
@@ -6,3 +6,9 @@ class Task:
     def __init__(self, name, due_date):
         self.name = name    # instance variable unique to each instance
         self.due_date = due_date
+
+    def __str__(self):
+        return u"Task name is: {0}, and Due Date is: {1}".format(self.name, self.due_date)
+
+    def __repr__(self):
+        return u"Task name is: {0}, and Due Date is: {1}".format(self.name, self.due_date)
